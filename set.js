a9c9b3c9a927ba618274c08dd040a7e5a4d46355
lium-dot <allenkangethe921@gmail.com>
2025-05-07 11:48:34 +0300

    Update set.js

--- set.js
+++ set.js
@@ -7,7 +7,7 @@ const fs = require('fs'),
 global.caption = "*©𝟐𝟎𝟐𝟒 𝐆𝐈𝐅𝐓𝐄𝐃 𝐌𝐃 𝐕𝟓*"; // Input Yours custom...(Maintain font for Flow)
 global.api = "https://api.giftedtech.web.id/api"; // DO NOT Change this...
 global.session = "https://pairing.giftedtech.web.id"; // DO NOT Change this....
-global.footer = "*ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɢɪғᴛᴇᴅ ᴛᴇᴄʜ*"; // Input Yours custom...(Maintain font for Flow)
+global.footer = "*ᴘσωєʀє∂ ву ★Ꭿ☼︎ℒ☼︎ℒ✩ℰ✫ℕ♫*"; // Input Yours custom...(Maintain font for Flow)
 global.ytdl = "https://ytdl.giftedtech.web.id"; // You Can Change this...
 global.giftedCdn = "https://cdn.giftedtech.web.id";
 global.sessionServer = "https://creds.giftedtech.web.id";
@@ -17,16 +17,16 @@ global.giftedApiRepo = "https://api.github.com/repos/mauricegift/gifted-md";
 
  
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Gifted~UDjKHDwPSlnNWBj8WPok", // Add sess Id here especially when deploying on panels else use app.json and .env file...
-SUDO_NUMBERS: process.env.SUDO_NUMBERS || "254778620961", //Add multiple Numbers with Country Codes without (+) Separated by Comma...
-ANTI_DELETE: process.env.ANTI_DELETE || true, // can be set to inboxonly/allchats/true/false
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+SESSION_ID: process.env.SESSION_ID || "Gifted~FN9sHTZWBqHIRNI9QtNu", // Add sess Id here especially when deploying on panels else use app.json and .env file...
+SUDO_NUMBERS: process.env.SUDO_NUMBERS || "254788757151", //Add multiple Numbers with Country Codes without (+) Separated by Comma...
+ANTI_DELETE: process.env.ANTI_DELETE || allchats, // can be set to inboxonly/allchats/true/false
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || "true",
-AUTO_LIKE_EMOJIS: process.env.AUTO_LIKE_EMOJIS || "💛,❤️,💜,🤍,💙", //Input Yours Custom...Can be one Emoji or Multiple Emojis Separated by Commas
+AUTO_LIKE_EMOJIS: process.env.AUTO_LIKE_EMOJIS || "💛,❤️,💜,🤍,💙,👀,👣,☠️", //Input Yours Custom...Can be one Emoji or Multiple Emojis Separated by Commas
 AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "false",
-STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || "✅️ Status Viewed by ★Ꭿ☼︎ℒ☼︎ℒ✩ℰ✫ℕ♫!", // // Input Yours custom...
-MODE: process.env.MODE || "public", // Put private or public or inbox or groups
-OWNER_NUMBER: process.env.OWNER_NUMBER || "254778620961", // Only 1 owner Number Here, others Add to sudo numbers...
+STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || " Ꭿℒℒℰℕ ℐЅ ᏇᎯTℂℋℐℕᎶ👀ℐℕ ᎶℋᎾЅT ℳᎾⅅℰ👻 ", // // Input Yours custom...
+MODE: process.env.MODE || "private", // Put private or public or inbox or groups
+OWNER_NUMBER: process.env.OWNER_NUMBER || "254758443111", // Only 1 owner Number Here, others Add to sudo numbers...
 OWNER_NAME: process.env.OWNER_NAME || "★Ꭿ☼︎ℒ☼︎ℒ✩ℰ✫ℕ♫", // Input Yours custom...(Maintain font for Flow)
 PACK_AUTHOR: process.env.PACK_AUTHOR || "𝐆𝐈𝐅𝐓𝐄𝐃 𝐓𝐄𝐂𝐇", // Added // Input Yours custom...
 PACK_NAME: process.env.PACK_NAME || "★Ꭿ☼︎ℒ☼︎ℒ✩ℰ✫ℕ♫💜", // Added // Input Yours custom...
@@ -36,7 +36,7 @@ ANTILINK: process.env.ANTILINK || "true", //  Enter true to kick automatically o
 ANTICALL: process.env.ANTICALL || "true",
 ANTIBAD: process.env.ANTIBAD || "true",
 BAD_WORDS: process.env.BAD_WORDS || "fuck, pussy, anus, idiot", // Add Yours Separated by Comma(will be deleted if ANTIBAD is set to true)
-ANTICALL_MSG: process.env.ANTICALL_MSG || "*_📞 Auto Call Reject Mode Active. 📵 No Calls Allowed!_*",
+ANTICALL_MSG: process.env.ANTICALL_MSG || " ★Ꭿ☼︎ℒ☼︎ℒ✩ℰ✫ℕ♫ ĪS ΛŦ ßƐΛSŦ MᎾDƐ ŔĪƓĤŦ ИᎾᏯ ƇΛИŦ ŔƐƇĪƐ√Ɛ YᎾƱŔ ƇΛĿĿ 📞📵 ",
 AUTO_REACT: process.env.AUTO_REACT || "false",
 BOT_NAME: process.env.BOT_NAME || "𝐆𝐈𝐅𝐓𝐄𝐃-𝐌𝐃", // Input Yours custom...(Maintain font for Flow)
 BOT_PIC: process.env.BOT_PIC || "https://i.ibb.co/cXrVMRgL/file-1131.jpg", // You can Replace with yours...
@@ -44,11 +44,11 @@ AUTO_AUDIO: process.env.AUTO_AUDIO || "false",
 AUTO_BIO: process.env.AUTO_BIO || "false",
 AUTO_BIO_QUOTE: process.env.AUTO_BIO_QUOTE || "Gifted creates things that creates other things",
 CHAT_BOT: process.env.CHAT_BOT || "false", // Put value to true to enablle for all chats only or inbox to ebanle in pm chats only or groups to enable in groups only else false
-WELCOME: process.env.WELCOME || "true",
-GOODBYE: process.env.GOODBYE || "true",    
+WELCOME: process.env.WELCOME || "false",
+GOODBYE: process.env.GOODBYE || "false",    
 AUTO_READ_MESSAGES: process.env.AUTO_READ_MESSAGES || "false", // Enter value to true for blueticking all messages, or commands for blueticking only commands else false
-AUTO_BLOCK: process.env.AUTO_BLOCK || "212,79", // Add Multiple Country Codes Separated by Comma...
-PRESENCE: process.env.PRESENCE || "online", // Choose one: typing, recording, online, null
+AUTO_BLOCK: process.env.AUTO_BLOCK || "", // Add Multiple Country Codes Separated by Comma...
+PRESENCE: process.env.PRESENCE || "typing", // Choose one: typing, recording, online, null
 TIME_ZONE: process.env.TIME_ZONE || "Africa/Nairobi", // Enter yours else leave blank if not sure
 };
 
