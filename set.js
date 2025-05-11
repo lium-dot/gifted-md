778a3171fc5c0b7977f083e15b79b6e71935e289
lium-dot <allenkangethe921@gmail.com>
2025-05-12 00:39:01 +0300

    Update set.js

--- set.js
+++ set.js
@@ -4,7 +4,7 @@ const fs = require('fs'),
       dotenv = fs.existsSync('config.env') ? require('dotenv').config({ path: '/.env' }) : undefined,
       convertToBool = (text, fault = 'true') => text === fault;
 
-global.caption = "*©𝟐𝟎𝟐𝟒 𝐆𝐈𝐅𝐓𝐄𝐃 𝐌𝐃 𝐕𝟓*"; // Input Yours custom...(Maintain font for Flow)
+global.caption = "*©𝟐𝟎𝟐𝟒 ★Ꭿ☼︎ℒ☼︎ℒ✩ℰ✫ℕ♫ 𝐍𝐄𝐖 𝐕𝐄𝐑𝐒𝐈𝐎𝐍😎*"; // Input Yours custom...(Maintain font for Flow)
 global.api = "https://api.giftedtech.web.id/api"; // DO NOT Change this...
 global.session = "https://pairing.giftedtech.web.id"; // DO NOT Change this....
 global.footer = "*ᴘσωєʀє∂ ву ★Ꭿ☼︎ℒ☼︎ℒ✩ℰ✫ℕ♫*"; // Input Yours custom...(Maintain font for Flow)
@@ -17,8 +17,8 @@ global.giftedApiRepo = "https://api.github.com/repos/mauricegift/gifted-md";
 
  
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Gifted~kuUhHwGL5f7ljtUpg1Ee", // Add sess Id here especially when deploying on panels else use app.json and .env file...
-SUDO_NUMBERS: process.env.SUDO_NUMBERS || "254788757151", //Add multiple Numbers with Country Codes without (+) Separated by Comma...
+SESSION_ID: process.env.SESSION_ID || "Gifted~3B2LoQw2PU3Jawg5GIpQ", // Add sess Id here especially when deploying on panels else use app.json and .env file...
+SUDO_NUMBERS: process.env.SUDO_NUMBERS || "254758443111", //Add multiple Numbers with Country Codes without (+) Separated by Comma...
 ANTI_DELETE: process.env.ANTI_DELETE || allchats, // can be set to inboxonly/allchats/true/false
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || "true",
@@ -28,18 +28,18 @@ STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || " Ꭿℒℒℰℕ ℐЅ ᏇᎯ
 MODE: process.env.MODE || "private", // Put private or public or inbox or groups
 OWNER_NUMBER: process.env.OWNER_NUMBER || "254758443111", // Only 1 owner Number Here, others Add to sudo numbers...
 OWNER_NAME: process.env.OWNER_NAME || "★Ꭿ☼︎ℒ☼︎ℒ✩ℰ✫ℕ♫", // Input Yours custom...(Maintain font for Flow)
-PACK_AUTHOR: process.env.PACK_AUTHOR || "𝐆𝐈𝐅𝐓𝐄𝐃 𝐓𝐄𝐂𝐇", // Added // Input Yours custom...
+PACK_AUTHOR: process.env.PACK_AUTHOR || "𝐀𝐋𝐋𝐄𝐍 𝐓𝐄𝐂𝐇", // Added // Input Yours custom...
 PACK_NAME: process.env.PACK_NAME || "★Ꭿ☼︎ℒ☼︎ℒ✩ℰ✫ℕ♫💜", // Added // Input Yours custom...
 PREFIX: process.env.PREFIX || ".",
 VERSION: process.env.VERSION || "5.0.0",
-ANTILINK: process.env.ANTILINK || "true", //  Enter true to kick automatically or delete to delete without kicking or warn to warn before kicking
+ANTILINK: process.env.ANTILINK || "false", //  Enter true to kick automatically or delete to delete without kicking or warn to warn before kicking
 ANTICALL: process.env.ANTICALL || "true",
 ANTIBAD: process.env.ANTIBAD || "true",
 BAD_WORDS: process.env.BAD_WORDS || "fuck, pussy, anus, idiot", // Add Yours Separated by Comma(will be deleted if ANTIBAD is set to true)
 ANTICALL_MSG: process.env.ANTICALL_MSG || " ★Ꭿ☼︎ℒ☼︎ℒ✩ℰ✫ℕ♫ ĪS ΛŦ ßƐΛSŦ MᎾDƐ ŔĪƓĤŦ ИᎾᏯ ƇΛИŦ ŔƐƇĪƐ√Ɛ YᎾƱŔ ƇΛĿĿ 📞📵 ",
 AUTO_REACT: process.env.AUTO_REACT || "false",
 BOT_NAME: process.env.BOT_NAME || "𝐆𝐈𝐅𝐓𝐄𝐃-𝐌𝐃", // Input Yours custom...(Maintain font for Flow)
-BOT_PIC: process.env.BOT_PIC || "https://i.ibb.co/cXrVMRgL/file-1131.jpg", // You can Replace with yours...
+BOT_PIC: process.env.BOT_PIC || "https://i.ibb.co/MyCXTBGc/file-1359.jpg", // You can Replace with yours...
 AUTO_AUDIO: process.env.AUTO_AUDIO || "false",
 AUTO_BIO: process.env.AUTO_BIO || "false",
 AUTO_BIO_QUOTE: process.env.AUTO_BIO_QUOTE || "Gifted creates things that creates other things",
